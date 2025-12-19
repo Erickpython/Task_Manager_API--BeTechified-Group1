3b48f377762318b73244be01b00587eb8fe945eb
Erickpython <erick.wambugu23@gmail.com>
2025-12-19 23:18:41 +0300

    RECTIFIED TASK STATUS TO INCLUDE PENDNG/COMPLETED

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ app.use(express.json());
 
 // IN MEMORY DATA STORE
 let tasks = [
-  { id: 1, title: 'Sample Task 1', description: "Sample Description 1", completed: false },
-  { id: 2, title: 'Sample Task 2', description: "Sample Description 2", completed: true },
-  { id: 3, title: 'Sample Task 3', description: "Sample Description 3", completed: false },
-  { id: 4, title: 'Sample Task 4', description: "Sample Description 4", completed: true },
-  { id: 5, title: 'Sample Task 5', description: "Sample Description 5", completed: false },
-
+  { id: 1, title: 'Sample Task 1', description: "Sample Description 1", status: "Pending" },
+  { id: 2, title: 'Sample Task 2', description: "Sample Description 2", status: "Completed" },
+  { id: 3, title: 'Sample Task 3', description: "Sample Description 3", status: "Pending" },
+  { id: 4, title: 'Sample Task 4', description: "Sample Description 4", status: "Pending" },
+  { id: 5, title: 'Sample Task 5', description: "Sample Description 5", status: "Completed" },
 ];
 
 
@@ -31,7 +30,7 @@ app.get('/tasks', (req, res) => {
 
 // GET COMPLETED TASKS
 app.get('/tasks/completed', (req, res) => {
-  const completedTasks = tasks.filter(t => t.completed);
+  const completedTasks = tasks.filter(t => t.status === "Completed");
   res.json(completedTasks);
 });
 
@@ -53,7 +52,7 @@ app.post('/tasks', (req, res) => {
     id: tasks.length + 1,
     title: req.body.title,
     description: req.body.description,
-    completed: req.body.completed || false
+    status: req.body.status || "Pending"
   };
   // Validate request body - check for missing fields
   if (!newTask.title || !newTask.description) {
